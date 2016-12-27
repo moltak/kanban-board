3a2371a8810bb240bff775f24810471bde718b56
moltak <moltak@gmail.com>
2016-12-28 01:23:08 +0900

    End of 2chapter
    what happened
    
    [ticket: X]

---
+++ app/Search.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+class Search extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchTerm: "React"
+        };
+    }
+
+    handleChange(event) {
+        this.setState({searchTerm: event.target.value});
+    }
+
+    render() {
+        return (
+            <div>
+                Search Term: <input type="search" value={this.state.searchTerm} onChange={this.handleChange.bind(this)}/>
+            </div>
+        );
+    }
+}
+
+render(<Search />, document.body);
