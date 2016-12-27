3f8d1b526581396e9d45e2c0aa44a2f0342732d8
moltak <moltak@gmail.com>
2016-12-27 14:54:36 +0900

    Event 함수 전역 변수로 변경
    what happened
    
    [ticket: X]

--- app/CheckList.js
+++ app/CheckList.js
@@ -2,17 +2,13 @@ import React, {Component} from 'react';
 
 class CheckList extends Component {
     render() {
-        console.log('tasks --> ');
-        console.log(this.props.tasks);
-        let tasks = this.props.tasks.map((task) => {
+        let tasks = this.props.tasks.map((task) => (
             <li calssName="checklist__task">
                 <input type="checkbox" defaultChecked={task.done} />
                 {task.name}
                 <a href="#" className="checklist__task--remove" />
             </li>
-        });
-        console.log('mapped --> ');
-        console.log(tasks);
+        ));
         return (
             <div className="checklist">
                 <ul>{tasks}</ul>
